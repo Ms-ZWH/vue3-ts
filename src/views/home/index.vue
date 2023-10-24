f09c53b6de5a6b88513dea991de302064fae83ce
Ms-ZWH <15835201575@163.com>
2023-10-24 13:13:57 +0800

    nav

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="common-layout">
-        <el-container>
+        <el-container class="container">
             <el-header ref="backColor">
                 <el-row :gutter="20">
                     <el-col :span="3">
@@ -9,25 +9,28 @@
                         </div>
                     </el-col>
                     <el-col :span="17">
-                        <div class="grid-content ep-bg-purple header_title text_color">
+                        <div class="grid-content ep-bg-purple header_title text_style">
                             后台管理系统
                         </div>
                     </el-col>
                     <el-col :span="4">
-                        <div class="grid-content ep-bg-purple header_right text_color">
+                        <div class="grid-content ep-bg-purple header_right text_style">
                             <div @click="handleChangeColor">换肤</div>
                             <div @click="handleExit">退出</div>
                         </div>
                     </el-col>
                 </el-row>
             </el-header>
+            <div class="header_bottom">
+                <div class="nav_switch">
+                    <el-icon @click="openNav">
+                        <Operation />
+                    </el-icon>
+                </div>
+            </div>
             <el-container>
-                <el-aside width="200px">
-                    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
-                        <el-radio-button :label="false">expand</el-radio-button>
-                        <el-radio-button :label="true">collapse</el-radio-button>
-                    </el-radio-group>
-                    <el-menu default-active="2" class="el-menu-vertical-demo" :collapse="isCollapse" @open="handleOpen"
+                <div class="nav">
+                    <el-menu default-active="2" class="el-menu-vertical-demo" :collapse="!isCollapse" @open="handleOpen"
                         @close="handleClose">
                         <el-sub-menu index="1">
                             <template #title>
@@ -36,17 +39,9 @@
                                 </el-icon>
                                 <span>Navigator One</span>
                             </template>
-                            <el-menu-item-group>
-                                <template #title><span>Group One</span></template>
-                                <el-menu-item index="1-1">item one</el-menu-item>
-                                <el-menu-item index="1-2">item two</el-menu-item>
-                            </el-menu-item-group>
-                            <el-menu-item-group title="Group Two">
-                                <el-menu-item index="1-3">item three</el-menu-item>
-                            </el-menu-item-group>
-                            <el-sub-menu index="1-4">
-                                <template #title><span>item four</span></template>
-                                <el-menu-item index="1-4-1">item one</el-menu-item>
+                            <el-sub-menu index="1-1">
+                                <template #title><span>item one</span></template>
+                                <el-menu-item index="1-1-1">item one-1</el-menu-item>
                             </el-sub-menu>
                         </el-sub-menu>
                         <el-menu-item index="2">
@@ -66,7 +61,7 @@
                             <template #title>Navigator Four</template>
                         </el-menu-item>
                     </el-menu>
-                </el-aside>
+                </div>
                 <el-container>
                     <el-main>Main</el-main>
                     <el-footer>Footer</el-footer>
@@ -84,9 +79,17 @@ import {
     Menu as IconMenu,
     Location,
     Setting,
-    Management
+    Operation
 } from '@element-plus/icons-vue';
 export default defineComponent({
+    // 注册组件
+    components: {
+        Document,
+        IconMenu,
+        Location,
+        Setting,
+        Operation
+    },
     setup() {
         // 初始化皮肤颜色
         window.document.documentElement.setAttribute('dataskin', 'skin1');
@@ -106,8 +109,11 @@ export default defineComponent({
             router.push('/login');
             localStorage.removeItem('token')
         }
-
-        const isCollapse = ref(true)
+        // 左侧导航栏收缩
+        const isCollapse = ref<boolean>(true)
+        const openNav = () => {
+            isCollapse.value = !isCollapse.value
+        }
         const handleOpen = (key: string, keyPath: string[]) => {
             console.log(key, keyPath)
         }
@@ -121,13 +127,25 @@ export default defineComponent({
             handleExit,
             themesAttributeArr,
             handleOpen,
-            handleClose
+            handleClose,
+            openNav,
+
         }
     }
 })
 </script>
 
 <style scoped lang="scss">
+.common-layout {
+    width: 100%;
+    height: 100%;
+
+    .container {
+        width: 100%;
+        height: 100vh;
+    }
+}
+
 .el-header {
     height: 80px;
 
@@ -161,10 +179,29 @@ export default defineComponent({
 }
 
 .el-container {
+    flex: 1;
+}
 
-    .el-menu-vertical-demo:not(.el-menu--collapse) {
-        width: 200px;
-        min-height: 400px;
-    }
+.nav {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+}
+
+.header_bottom {
+    width: 100%;
+    height: 40px;
+    display: flex;
+    align-items: center;
+}
+
+.el-menu {
+    flex: 1;
+}
+
+.nav_switch {
+    // height:100%
+    color: #fff;
+    font-size: 28px;
 }
 </style>
